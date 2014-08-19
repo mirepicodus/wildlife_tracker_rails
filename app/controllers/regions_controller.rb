c2cf4d13536ee4d5933f30f5481ce79d563cfe27
moof mayeda & mark rubenstein <student@epicodus.com>
2014-08-19 11:00:44 -0700

    create new regions

--- app/controllers/regions_controller.rb
+++ app/controllers/regions_controller.rb
@@ -4,4 +4,10 @@ class RegionsController < ApplicationController
     @region = Region.new
     render('/regions/index.html.erb')
   end
+
+  def create
+    @regions = Region.all
+    @region = Region.create(params[:region])
+    render('/regions/index.html.erb')
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   match('/', {:via => :get, :to => 'animals#index'})
   match('/', {:via => :post, :to => 'animals#create'})
   match('/regions/', {via: :get, to: 'regions#index'})
+  match('/regions/', {via: :post, to: 'regions#create'})
   match('/:id', {:via => :get, :to => 'animals#show'})
   match('/:id', {:via => [:patch, :put], :to => 'animals#update'})
   match('/:id', {:via => :delete, :to => 'animals#destroy'})
